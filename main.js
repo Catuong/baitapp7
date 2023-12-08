38effca6c32e554bd860bd65d93740262a0a5377
Catuong <148960507+Catuong@users.noreply.github.com>
2023-12-08 22:10:55 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+// 1;
+// const numbers = [1, 2, 3, 4, 5, 6];
+// function getEvenNumbers(array) {
+//   const count = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 0) {
+//       cart.push(array[i]);
+//     }
+//   }
+//   return count;
+// }
+// console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// 2;
+// const numbers = [1, 2, 3, 4, 5, 6];
+// function getOddNumbersLength(array) {
+//   const odds = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 !== 0) {
+//       odds.push(array[i]);
+//     }
+//   }
+
+//   return odds.length;
+// }
+
+// 3;
+// // const numbers = [5, 1, 9, 3, 10, 4];
+// function getOddNumbersLength(array) {
+//   const odds = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 !== 0) {
+//       odds.push(array[i]);
+//     }
+//   }
+
+//   return odds.length;
+// }
+
+// // console.log(maxNumber([5, 1, 9, 3, 10, 4]));
+
+// 4;
+// // const numbers = [5, 1, 9, 3, 10, 4];
+// function findMinNumber(array) {
+//   let minHeight = array[0];
+//   for (let i = 1; i < array.length; i++) {
+//     if (array[i] < minHeight) {
+//       minHeight = array[i];
+//     }
+//   }
+//   return minHeight;
+// }
+// // 
+// // console.log(maxNumber([5, 1, 9, 3, 10, 4]));
+
+// // 5.
+// // function check_arr(arr){
+// //     let count = 0;
+// //     for (let i = 0; i < arr.length; i ++){
+// //         if (arr[i] == 5)  {
+// //          return true;
+// //         }
+// //     }
+// //  return false;
+// // }
+// // let arr = [1, 3, 5];
+
+// // console.log(check_arr("5", arr));
+
+// // // 6.
+// // function concatenate(array) {
+// //   let result = "";
+
+// //   for (let i = 0; i < array.length; i++) {
+// //     result = result + array[i];
+// //   }
+
+// //   return result;
+// // }
+
+// // 7.
+// // const num = [1, 2, 3];
+// // function changeArr(array) {
+// //   let result = [];
+
+// //   for (let i = 0; i < array.length; i++) {
+// //     result.push((array[i]));
+// //   }
+
+// //   return result + "";
+// // }
+// // console.log(changeArr([1, 2, 3]));
+
+// 8.
+// const numbers = [1, 2, 3];
+// function sumArray(array) {
+//   let sum = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+//   }
+
+//   return sum;
+// }
+// console.log(sumArray([1, 2, 3]));
+
+// 9.
+// let arr = [1, 2, 3, 4, 5];
+//     for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === 2) {
+//         arr.splice(i, 1);
+//     }
+// }
+// console.log(arr);
+
+// const number = [1,3,5];
+
+// 10.
+// // const numbers1 = [1, 2, 3];
+// // const numbers2 =  [4, 5, 6];
+// // console.log(numbers1.concat(numbers2))
